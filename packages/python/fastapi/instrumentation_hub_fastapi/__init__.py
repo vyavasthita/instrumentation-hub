8680d29cbea385daf8f6b3308728c6936ec6629c
Dilip Sharma <diliplakshya@gmail.com>
2026-01-04 09:48:44 +0530

    Remove unused method

--- packages/python/fastapi/instrumentation_hub_fastapi/__init__.py
+++ packages/python/fastapi/instrumentation_hub_fastapi/__init__.py
@@ -1,6 +1,6 @@
 """Public exports for the Instrumentation Hub FastAPI adapter."""
 
-from .bootstrap import FastAPIInstrumentation, InstrumentationResult, setup_fastapi_instrumentation
+from .bootstrap import FastAPIInstrumentation, InstrumentationResult
 from .enums.log_level import LogLevel
 from .config import Config, ConfigModel
 from .observability.otel_collector.metrics.middleware import MetricsMiddleware
@@ -14,6 +14,5 @@ __all__ = [
 	"ConfigModel",
 	"MetricsMiddleware",
 	"rate_limited_log",
-	"setup_fastapi_instrumentation",
 	"LogLevel",
 ]
